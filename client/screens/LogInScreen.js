960242e091c4d1bbab052b9a43e5014897bb1071
j-tw <jacob@weinmanemail.com>
2020-02-04 14:34:41 -0500

    Added Login button

--- client/screens/LogInScreen.js
+++ client/screens/LogInScreen.js
@@ -14,16 +14,21 @@ import {
 export default function LogInScreen() {
     return (
         <View style={{paddingTop:80}}>
-        <Text style={styles.welcometext}>
-          Welcome to
-        </Text>
-        <Text style={styles.triDiet}>
-         TriDiet
-        </Text>
-        <Text style={styles.welcometext}>
-          a better way to track your health
-        </Text>
+          <Text style={styles.welcometext}>
+            Welcome to
+          </Text>
+          <Text style={styles.triDiet}>
+           TriDiet
+          </Text>
+          <Text style={styles.welcometext}>
+            a better way to track your health
+          </Text>
+
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Log In</Text>
+          </View>
         </View>
+
     );
 }
 
@@ -33,18 +38,33 @@ LogInScreen.navigationOptions = {
 
 const styles = StyleSheet.create({
     welcometext: {
-        paddingTop: 30,
+        padding: 30,
         fontSize: 27,
         color: 'rgba(96,100,109, 1)',
         textAlign: 'center',
         fontFamily: 'space-mono',
       },
       triDiet: {
-        paddingTop: 30,
+        padding: 30,
         fontWeight: '900',
-        fontSize: 27,
+        fontSize: 40,
         color: '#0644F9',
         textAlign: 'center',
         fontFamily: 'space-mono',
       },
+
+      button: {
+    alignItems: 'center',
+    backgroundColor: '#0644F9',
+    borderRadius: 10,
+  },
+  buttonText: {
+    textAlign: 'center',
+    paddingTop: 12,
+    paddingBottom: 12,
+    paddingLeft: 30,
+    paddingRight: 30,
+    color: 'white',
+    fontSize: 18,
+  },
 });
